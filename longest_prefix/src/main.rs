105329d30020ec8c54b58dad2bce2995266b9bb7
loi nguyen <loinv226@gmail.com>
2021-11-18 01:23:56 +0700

    Update: syntax, get char by index

--- longest_prefix/src/main.rs
+++ longest_prefix/src/main.rs
@@ -1,45 +1,17 @@
 
 pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    let mut result: Vec<char> = vec![];
-    let mut last_valid_index: i8 = 0;
-   
-    'rootLoop: for (i, elem) in strs.iter().enumerate() {
-        for (j, value) in elem.chars().enumerate() {
-            let j_index = j as i8;
-            if i == 0 {
-                // first work alway valid
-                result.push(value);
-                last_valid_index = j_index;
-                continue
-            }
-            
-            if let Some(exist) = result.get(j) {
-                if *exist != value {
-                    // not same prefix
-                    last_valid_index = j_index - 1;
-                    
-                    if last_valid_index <= 0 {
-                        break 'rootLoop;
-                    }
-                }
-            }
-
-            if j_index >= last_valid_index {
-                break;
-            }
-        }
-    }
     let mut output = String::from("");
 
-    if last_valid_index < 0 {
-        return output;
-    }
-    
-    for (index, elem) in result.iter().enumerate() {
-        if (index as i8) > last_valid_index {
-            break;
+    if let Some(first) = strs.get(0) {
+        for (i, c) in first.chars().enumerate() {
+            // loop other word
+            for other in strs.iter() {
+                if other.len() <= i || c != other.chars().nth(i).unwrap() {
+                    return output;
+                }
+            }
+            output.push(c);
         }
-        output.push(*elem);
     }
     return output;
 }
